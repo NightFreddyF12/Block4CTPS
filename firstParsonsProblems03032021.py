5f619a477b834d3b26662459b66dabd73ad56512
NightFreddyF12 <79872661+NightFreddyF12@users.noreply.github.com>
2021-03-03 13:00:03 -0700

    Add files via upload

---
+++ firstParsonsProblems03032021.py
@@ -0,0 +1,34 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+
+#Write your answer to Parson's Problem #1 below:
+#basic for loop
+for x in 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
+    print("hello world!")
+
+#Write your answer to Parson's Problem #2 below:
+#basic list
+myList = ["apples", "pineapples"]
+myList.append("pears")
+myList.reverse()
+print(myList)
+
+#Write your answer to Parson's Problem #3 below:
+#basic conditional
+x = input("Type a number    ")
+x = int(x)
+if x > 0:
+    print("That're more than nothing!")
+else:
+    print("That's a small number!")
