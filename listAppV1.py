13ad2aa3967c992589f8ba4a82eed26495764294
NightFreddyF12 <79872661+NightFreddyF12@users.noreply.github.com>
2021-03-09 13:27:43 -0700

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,30 @@
+"""
+Program Goals:
+1. Get input from the user (at multiple points)
+2. We need to convert some of this input to INTs from STRs
+3. We need to provide choices to the user
+    a. Add more values to a list
+    b. Return a value at a specific index
+
+"""
+
+def mainProgram():
+    myList = []
+    print("Hello, there! Let's work with lists!")
+    print("Choose from the following options. Type a number below!")
+    choice = input("1. Add to a list , 2. Return the value at an index position")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+    #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
+
+def addToList():
+    print("Adding to a list! Great choice!")
+    newItem = input("Type an integer here!    ")
+    myList.append(int(newItem))
+
+def indexValues():
+
+if __name__ == "__main__":
+    mainProgram()
