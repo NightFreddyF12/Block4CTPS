9bcd76553ae6cfb09e12cf3fcd3a96f790dd6cfd
NightFreddyF12 <79872661+NightFreddyF12@users.noreply.github.com>
2021-03-11 13:23:18 -0700

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -1,6 +1,6 @@
 """
 Program Goals:
-1. Get input from the user (at multiple points)
+1. Get input from the user (at multiple points) 
 2. We need to convert some of this input to INTs from STRs
 3. We need to provide choices to the user
     a. Add more values to a list
@@ -9,15 +9,21 @@ Program Goals:
 """
 
 def mainProgram():
-    myList = []
-    print("Hello, there! Let's work with lists!")
-    print("Choose from the following options. Type a number below!")
-    choice = input("1. Add to a list , 2. Return the value at an index position")
-    if choice == "1":
-        addToList()
-    elif choice == "2":
-        indexValues()
-    #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
+    while true:
+        myList = []
+        print("Hello, there! Let's work with lists!")
+        print("Choose from the following options. Type a number below!")
+        choice = input("""1. Add to a list or
+    2. Return the value at an index position
+    3. Exit Program   """)
+        #add a way to catch bad user input
+        if choice == "1":
+            addToList()
+        elif choice == "2":
+            indexValues()
+        elif choice == "3":
+            break
+        #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
 
 def addToList():
     print("Adding to a list! Great choice!")
@@ -25,6 +31,12 @@ def addToList():
     myList.append(int(newItem))
 
 def indexValues():
+    print("Ohhh! I heard you need a particular piece of data!")
+    indexPos = input("What index position are you curious about?   ")
+    myList[int(indexPos)]
+    print(myList[int(indexPos)])
 
+
+#dunder main -> Double Underscore---dunder
 if __name__ == "__main__":
     mainProgram()
