099696398af4d27556bb82720ecba7f6c4daf76a
NightFreddyF12 <79872661+NightFreddyF12@users.noreply.github.com>
2021-03-12 13:11:45 -0700

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -9,20 +9,26 @@ Program Goals:
 """
 
 def mainProgram():
-    while true:
-        myList = []
-        print("Hello, there! Let's work with lists!")
-        print("Choose from the following options. Type a number below!")
-        choice = input("""1. Add to a list or
-    2. Return the value at an index position
-    3. Exit Program   """)
-        #add a way to catch bad user input
-        if choice == "1":
-            addToList()
-        elif choice == "2":
-            indexValues()
-        elif choice == "3":
-            break
+    #build a while loop here!
+    while True:
+        try:
+            print("Hello, there! Let's work with lists!")
+            print("Choose from the following options. Type a number below!")
+            choice = input("""1. Add to a list or
+2. Return the value at an index position
+3. Random search
+4. Exit Program   """)
+            #add a way to catch bad user input
+            if choice == "1":
+                addToList()
+            elif choice == "2":
+                indexValues()
+            elif choice == "3":
+                randomSearch()
+            elif choice == "4":
+                break
+        except:
+            print("You did a dumb! Did you do it right?")
         #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
 
 def addToList():
@@ -33,8 +39,11 @@ def addToList():
 def indexValues():
     print("Ohhh! I heard you need a particular piece of data!")
     indexPos = input("What index position are you curious about?   ")
-    myList[int(indexPos)]
     print(myList[int(indexPos)])
+
+def randomSearch():
+    print("RaNdOm SeArCh?!?")
+    print(myList[random.randint(0, len(myList)-1)])
 
 
 #dunder main -> Double Underscore---dunder
