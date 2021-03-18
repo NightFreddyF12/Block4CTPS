410ea19cabd541bc7e4895815be3ae719584d11e
NightFreddyF12 <79872661+NightFreddyF12@users.noreply.github.com>
2021-03-18 13:24:00 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -9,32 +9,43 @@ Program Goals:
 """
 
 def mainProgram():
-    #build a while loop here!
     while True:
         try:
             print("Hello, there! Let's work with lists!")
             print("Choose from the following options. Type a number below!")
             choice = input("""1. Add to a list or
-2. Return the value at an index position
-3. Random search
-4. Exit Program   """)
-            #add a way to catch bad user input
+2. Add a bunch of numbers
+3. Return the value at an index
+4. Random search
+5. Print contents of list
+6. Quit Program   """)
             if choice == "1":
                 addToList()
             elif choice == "2":
-                indexValues()
+                addABunch()
             elif choice == "3":
-                randomSearch()
+                indexValues()
             elif choice == "4":
+                randomSearch()
+            elif choice == "5":
+                print(myList)
+            else:
                 break
         except:
             print("You did a dumb! Did you do it right?")
-        #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
 
 def addToList():
     print("Adding to a list! Great choice!")
     newItem = input("Type an integer here!    ")
     myList.append(int(newItem))
+    
+def addABunch():
+    print("We're gonna add a bunch of integers here!")
+    numToAdd = input("How many integers would you like to add?   ")
+    numRange = input("And how high would you like these numbers to go?   ")
+    for x in range(0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
+    print("Your list is now complete.")
 
 def indexValues():
     print("Ohhh! I heard you need a particular piece of data!")
@@ -45,7 +56,13 @@ def randomSearch():
     print("RaNdOm SeArCh?!?")
     print(myList[random.randint(0, len(myList)-1)])
 
+def linearSearch():
+    print("We're gonna check out each item one at a time in your list! This sucks.")
+    searchItem = input("Whatcha lookin' for, pardner?")
+    for x in range(len(myList)):
+        if myList[x] == int(searchItem):
+            print("Your item is at inder position ()".format(x))
 
-#dunder main -> Double Underscore---dunder
+
 if __name__ == "__main__":
     mainProgram()
